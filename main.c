9d01efb236698692e8e81ce6f52c852e46872334
haeenah <haeenah629@sookmyung.ac.kr>
2026-08-16 01:18:00 +0900

    실습 5 완료

--- main.c
+++ main.c
@@ -1,30 +1,46 @@
 #include <stdio.h>
-#define SIZE 4
 
-void square_array(int a[], int size);
-void print_array(int a[], int size);
+#define ROWS 3
+#define COLS 3
+
+// 두 행렬을 더하는 함수 (C = A + B)
+void addMatrix(int A[][COLS], int B[][COLS], int C[][COLS]) {
+    int r, c;
+    for (r = 0; r < ROWS; r++) {
+        for (c = 0; c < COLS; c++) {
+            C[r][c] = A[r][c] + B[r][c];
+        }
+    }
+}
+
+// 행렬을 출력하는 함수
+void printMatrix(int A[][COLS]) {
+    int r, c;
+    for (r = 0; r < ROWS; r++) {
+        for (c = 0; c < COLS; c++) {
+            printf("%3d ", A[r][c]);
+        }
+        printf("\n");
+    }
+}
 
 int main(void) {
-    int list[SIZE] = {1, 2, 3, 4};
+    int A[ROWS][COLS] = {
+        {2, 3, 0},
+        {8, 9, 1},
+        {7, 0, 5}
+    };
 
-    print_array(list, SIZE);
-    square_array(list, SIZE);
-    print_array(list, SIZE);
+    int B[ROWS][COLS] = {
+        {1, 0, 0},
+        {0, 1, 0},
+        {0, 0, 1}
+    };
+
+    int C[ROWS][COLS]; // 덧셈 결과 행렬
+
+    addMatrix(A, B, C);
+    printMatrix(C);
 
     return 0;
-}
-
-void square_array(int a[], int size) {
-    int i;
-    for (i = 0; i < size; i++) {
-        a[i] = a[i] * a[i];
-    }
-}
-
-void print_array(int a[], int size) {
-    int i;
-    for (i = 0; i < size; i++) {
-        printf("%3d", a[i]);
-    }
-    printf("\n");
 }
