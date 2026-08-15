deaec14a8cc46c5f7e497924f5894a29e4daef48
haeenah <haeenah629@sookmyung.ac.kr>
2026-08-16 01:12:28 +0900

    실습 3 완료

--- main.c
+++ main.c
@@ -1,21 +1,25 @@
-#include <stdio.h>
-#include <stdlib.h> //
+#include <stdlib.h>
 #define SIZE 5
 
 int main(void) {
-    system("chcp 65001"); 
-    int i, average;
-    int sum = 0;
+    int i;
     int grade[SIZE];
+    int score[SIZE];
 
+    // grade 배열에 난수 저장
     for (i = 0; i < SIZE; i++) {
-        printf("학생 성적을 입력하세요 : ");
-        scanf("%d", &grade[i]);
-        sum += grade[i];
+        grade[i] = rand() % 100;
     }
 
-    average = sum / SIZE;
-    printf("성적 평균 : %d\n", average);
+    // 배열 값 복사 (grade -> score)
+    for (i = 0; i < SIZE; i++) {
+        score[i] = grade[i];
+    }
+
+    // 복사된 score 배열 출력
+    for (i = 0; i < SIZE; i++) {
+        printf("score[%d] = %d\n", i, score[i]);
+    }
 
     return 0;
 }
