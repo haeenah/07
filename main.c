1ffe936973c299214f727ad75c25e013dac6c3ff
haeenah <haeenah629@sookmyung.ac.kr>
2026-08-29 15:19:20 +0900

    실습 2 완료

--- main.c
+++ main.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 
 int main(void) {
-    int flag = 1;
-    int y; // y의 범위를 main 함수 전체로 확대
+    int i;
 
-    while (flag != 0) {
-        y = 3;
-        flag = 0;
+    for (i = 0; i < 5; i++) {
+        int temp = 1;
+        printf("temp = %d\n", temp);
+        temp++;
     }
-
-    y = 4; // y가 main 함수 지역 변수이므로 접근 가능!
 
     return 0;
 }
